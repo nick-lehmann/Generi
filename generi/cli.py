569a1ff15a44b59e4ece10c6a1522d269edc1def
Nick Lehmann <nick@lehmann.sh>
2019-12-22 23:54:50 +0100

    Remove async building

--- generi/cli.py
+++ generi/cli.py
@@ -5,9 +5,5 @@ from .generi import Generi
 
 def main():
     generator = Generi(sys.argv[1])
-    generator.write()
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(generator.build())
-    loop.close()
-
+    # generator.write()
+    generator.build()

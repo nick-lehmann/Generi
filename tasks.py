39b64bcc821cec67642936c7266b60c45339c709
Nick Lehmann <nick@lehmann.sh>
2019-12-26 17:20:01 +0100

    Add invoke command to build documentation, including docker images

---
+++ tasks.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+from invoke import task
+
+try:
+    from sphinx.cmd.build import build_main as sphinx_build
+except ImportError:
+    sphinx_build = None
+
+
+@task()
+def docs(context, image=False):
+    """ Generate the documentation """
+    if not sphinx_build:
+        raise ValueError('''
+            The sphinx build command is not available. 
+            You have probably not installed sphinx in your environment
+        ''')
+
+    print('Start building docs')
+    working_dir = os.getcwd()
+    source_dir = os.path.join(working_dir, 'docs')
+    target_dir = os.path.join(working_dir, 'dist/docs')
+
+    sphinx_build(argv=[
+        source_dir,
+        target_dir
+    ])
+
+    if image:
+        subprocess.run('docker build -t nicklehmann/generi_docs -f docs/Dockerfile .', shell=True)
